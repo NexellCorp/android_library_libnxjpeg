8cc34918b76fe2c60e70e29389882acabbbf5900
Sungwoo Park <swpark@nexell.co.kr>
2017-08-24 13:35:52 +0900

    Introduce libnxjpeg
    This patch introduces libnxjpeg that is software jpeg encoding library
    using libjpeg_turbo.
    Nexell Camera buffer output has stride align factor so, needs this
    extended library.
    
    Change-Id: I0bbd554ddcde0a77bff951f963e3c31bc98e1326
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

---
+++ Android.mk
@@ -0,0 +1,21 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+#LOCAL_PRELINK_MODULE := false
+
+#LOCAL_MODULE_TAGS := optional
+
+LOCAL_SHARED_LIBRARIES := liblog libcutils libjpeg
+
+LOCAL_C_INCLUDES += \
+	$(LOCAL_PATH)	\
+	external/libjpeg-turbo\
+	$(call include-path-for)
+
+LOCAL_CFLAGS += -DANDROID
+
+LOCAL_SRC_FILES:= libnxjpeg.c
+
+LOCAL_MODULE:= libnxjpeg
+
+include $(BUILD_SHARED_LIBRARY)
